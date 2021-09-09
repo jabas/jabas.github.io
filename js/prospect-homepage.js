4fa12db4fb244fc47f30c1da7bee1a546cb4a6a8
Josh Jabas <71098277+jjabas@users.noreply.github.com>
2021-09-09 10:45:39 -0500

    ci in and out

--- js/prospect-homepage.js
+++ js/prospect-homepage.js
@@ -17,29 +17,51 @@
 		});
 	}
 
-	function chooseMosaic(event) {
-		var elClasses = event.target.classList;
-		var current = document.querySelector('.frame-selected')
-		var selected = '';
-		for (let i = 0; i < elClasses.length; i++) {
-			let className = elClasses[i];
-			if (className.startsWith('frame-') && className !== 'frame-selected') {
-				selected = className.replace('frame-', '');
+	function openMosaic(event) {
+		if(!document.querySelector('.frame-selected')) {
+			var elClasses = event.target.classList;
+			var current = document.querySelector('.frame-selected');
+			var selected = '';
+			for (let i = 0; i < elClasses.length; i++) {
+				let className = elClasses[i];
+				if (className.startsWith('frame-') && className !== 'frame-selected') {
+					selected = className.replace('frame-', '');
+				}
 			}
-		}
-		if (current) {
-			current.classList.remove('frame-selected');
-		}
-		event.target.classList.add('frame-selected');
+			event.target.classList.add('frame-selected');
 
+			
+			mosaic.classList.add('selected-' + selected, 'selection-active');
+			addBackdrop();
+		}
+	}
+
+	function closeMosaic() {
+		var current = document.querySelector('.frame-selected');
+		current.style.zIndex = 1050;
+		current.classList.remove('frame-selected');
 		for (let i = 0; i < mosaic.classList.length; i++) {
 			let className = mosaic.classList[i];
 			if (className.startsWith('selected-')) {
 				mosaic.classList.remove(className);
 			}
 		}
-		mosaic.classList.add('selected-' + selected);
-		mosaic.classList.add('selection-active');
+		mosaic.classList.remove('selection-active');
+		removeBackdrop(current);
+	}
+
+	function addBackdrop() {
+		body.appendChild(modalBg);
+		modalBg.offsetHeight; // force repaint
+		modalBg.classList.add('show');
+	}
+
+	function removeBackdrop(current) {
+		modalBg.classList.remove('show');
+		setTimeout(function(){
+			body.removeChild(modalBg);
+			current.style.zIndex = null;
+		}, 500);
 	}
 
 	var observer = new IntersectionObserver(intersectionCallback, intersectionOptions);
@@ -56,14 +78,23 @@
 	var frames = document.querySelectorAll('.frame');
 
 	for (let i = 0; i < frames.length; i++) {
-		frames[i].addEventListener('click', chooseMosaic);
+		frames[i].addEventListener('click', openMosaic);
 	}
 
 	var video = document.getElementById("bannerVideo");
+	if (video) {
+		video.addEventListener('ended', function(){
+			video.currentTime = 2.8;
+			video.play();
+		});
+	}
 
-	video.addEventListener('ended', function(){
-		video.currentTime = 2.7;
-		video.play();
-	});
+
+	var body = document.querySelector('body');
+	var modalBg = document.createElement('div');
+	var closeBtn = document.getElementById("mosaicClose");
+	modalBg.classList.add('modal-backdrop', 'fade');
+	modalBg.addEventListener('click', closeMosaic);
+	closeBtn.addEventListener('click', closeMosaic);
 })();
 //# sourceMappingURL=prospect-homepage.js.map
