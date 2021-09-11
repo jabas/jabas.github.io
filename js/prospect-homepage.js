4b7db89bf871edd4e98a5fde1b79939b88c2212f
Josh Jabas <71098277+jjabas@users.noreply.github.com>
2021-09-11 16:37:08 -0500

    SMIL animation

--- js/prospect-homepage.js
+++ js/prospect-homepage.js
@@ -122,50 +122,21 @@
 	var intersectionOptions = {
 		root: null,  // use the viewport
 		rootMargin: '0px',
-		threshold: 0.02
+		threshold: 0.30
 	}
 
 	function enteredZone(entries, observer) {
 		entries.forEach(entry => {
 			if (entry.isIntersecting) {
-				unitsEl.classList.remove('zone-active');
-			} else {
 				unitsEl.classList.add('zone-active');
+			} else {
+				unitsEl.classList.remove('zone-active');
 			}
 		});
 	}
 
-	var enteredZoneObserver = new IntersectionObserver(enteredZone, intersectionOptions);
-
-	var banner = document.querySelector('#mainBanner');
-	var header = document.querySelector('#headerNav');
 	var unitsEl = document.querySelector('#businessUnits');
-
-	if (banner) {
-		enteredZoneObserver.observe(banner);
-	}
-
-	var mosaic = document.querySelector('.mosaic');
-	var frames = document.querySelectorAll('.frame');
-
-	for (let i = 0; i < frames.length; i++) {
-		frames[i].addEventListener('click', openMosaic);
-	}
-
-	var video = document.getElementById("bannerVideo");
-	if (video) {
-		video.addEventListener('ended', function(){
-			video.currentTime = 2.8;
-			video.play();
-		});
-	}
-
-
-	var body = document.querySelector('body');
-	var modalBg = document.createElement('div');
-	var closeBtn = document.getElementById("mosaicClose");
-	modalBg.classList.add('modal-backdrop', 'fade');
-	modalBg.addEventListener('click', closeMosaic);
-	closeBtn.addEventListener('click', closeMosaic);
+	var enteredZoneObserver = new IntersectionObserver(enteredZone, intersectionOptions);
+	enteredZoneObserver.observe(unitsEl);
 })();
 //# sourceMappingURL=prospect-homepage.js.map
