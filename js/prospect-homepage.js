361964aafbd57d4eaf407a694ca949953fe361e8
Josh Jabas <71098277+jjabas@users.noreply.github.com>
2021-09-13 15:56:54 -0500

    mobile heights suck

--- js/prospect-homepage.js
+++ js/prospect-homepage.js
@@ -1,4 +1,12 @@
 (function() {
+	//adjustable 100vh to account for mobile chrome
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+	// window.addEventListener('resize', () => {
+	// 	let vh = window.innerHeight * 0.01;
+	// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
+	// });
 	// var video = document.getElementById("bannerVideo");
 	// if (video) {
 	// 	video.addEventListener('ended', function(){
