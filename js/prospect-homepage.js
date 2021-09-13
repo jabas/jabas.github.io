c2b3ef03e16accc0ee03dda2c1c200efd212da33
Josh Jabas <71098277+jjabas@users.noreply.github.com>
2021-09-13 11:43:40 -0500

    video testing

--- js/prospect-homepage.js
+++ js/prospect-homepage.js
@@ -1,11 +1,11 @@
 (function() {
-	var video = document.getElementById("bannerVideo");
-	if (video) {
-		video.addEventListener('ended', function(){
-			video.currentTime = 2.8;
-			video.play();
-		});
-	}
+	// var video = document.getElementById("bannerVideo");
+	// if (video) {
+	// 	video.addEventListener('ended', function(){
+	// 		video.currentTime = 2.8;
+	// 		video.play();
+	// 	});
+	// }
 })();
 (function() {
 
--- js/shared/banner.js
+++ js/shared/banner.js
@@ -1,9 +1,9 @@
 (function() {
-	var video = document.getElementById("bannerVideo");
-	if (video) {
-		video.addEventListener('ended', function(){
-			video.currentTime = 2.8;
-			video.play();
-		});
-	}
+	// var video = document.getElementById("bannerVideo");
+	// if (video) {
+	// 	video.addEventListener('ended', function(){
+	// 		video.currentTime = 2.8;
+	// 		video.play();
+	// 	});
+	// }
 })();
