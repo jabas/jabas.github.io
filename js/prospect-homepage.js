28e0eff20462f7273cd51f40959b175c84946aae
Josh Jabas <71098277+jjabas@users.noreply.github.com>
2021-09-30 10:32:40 -0500

    week one updates

--- js/prospect-homepage.js
+++ js/prospect-homepage.js
@@ -28,7 +28,7 @@
 	// switch out video if landscape page
 	if (isLandscape) {
 		banner.classList.add('banner-landscape');
-		banner.setAttribute('poster', '/images/prospect/lt-banner-landscape.jpg')
+		video.setAttribute('poster', '/images/prospect/lt-banner-landscape.jpg')
 		source.setAttribute('src', '/images/prospect/lt-banner-landscape.mp4');
 		video.load();
 	}
