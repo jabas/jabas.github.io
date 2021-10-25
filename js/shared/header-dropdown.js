53d53de40984728a5a27ac52268e6efbf8ce2f00
Josh Jabas <71098277+jjabas@users.noreply.github.com>
2021-10-25 12:29:33 -0500

    switch over to bs footer header

---
+++ js/shared/header-dropdown.js
@@ -0,0 +1,16 @@
+(function() {
+
+	const dropdownElementList = [].slice.call(document.querySelectorAll('.lt-header .dropdown-toggle'));
+	const dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+		return new bootstrap.Dropdown(dropdownToggleEl, {
+			popperConfig: function (defaultBsPopperConfig) {
+				let newPopperConfig = defaultBsPopperConfig;
+				if (defaultBsPopperConfig.placement === 'bottom-start') {
+					newPopperConfig.placement = 'bottom';
+				}
+				return newPopperConfig
+			}
+		});
+	});
+	
+})();
