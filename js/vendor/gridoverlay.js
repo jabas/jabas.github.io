16d103904bb3f823358598f09058ac3349eeff0c
Josh Jabas <71098277+jjabas@users.noreply.github.com>
2022-06-20 13:48:31 -0500

    return to storybook

--- js/vendor/gridoverlay.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-	// Detect option + G (alt + G on PC) keypress to toggle the grid overlay
-	$('body').on('keyup',function(e){
-		if(e.which==71 && e.altKey){
-			$('.sg-grid-overlay').toggleClass('invisible');
-		}
-	});
-});
